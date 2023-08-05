34b492597ee6a0d9faf0469e1e5cb7068581cafe
Dineshpalappdeveloper <dineshkr748199@gmail.com>
2023-08-05 07:41:03 +0530

    temp

--- src/Components/RouteCompo.js
+++ src/Components/RouteCompo.js
@@ -10,43 +10,43 @@ import Contactus from './Contactus'
 import EditStudent from './EditStudent'
 
 const RouteComp = () => {
-  const [Data,setData] = useState([
-    {Name : 'John',   Age: 24,  Course : 'MERN',  Batch : 'October',  },
-    {Name : 'Doe',    Age: 25,  Course : 'MERN',  Batch : 'November', },
-    {Name : 'Biden',  Age: 26,  Course : 'MERN',  Batch : 'September',},
-    {Name : 'Barar',  Age: 22,  Course : 'MERN',  Batch : 'September',},
-    {Name : 'Christ', Age: 23,  Course : 'MERN',  Batch : 'October',  },
-    {Name : 'Elent',  Age: 24,  Course : 'MERN',  Batch : 'November', }
-])
+  const [Data, setData] = useState([
+    { Name: 'John', Age: 24, Course: 'MERN', Batch: 'October', },
+    { Name: 'Doe', Age: 25, Course: 'MERN', Batch: 'November', },
+    { Name: 'Biden', Age: 26, Course: 'MERN', Batch: 'September', },
+    { Name: 'Barar', Age: 22, Course: 'MERN', Batch: 'September', },
+    { Name: 'Christ', Age: 23, Course: 'MERN', Batch: 'October', },
+    { Name: 'Elent', Age: 24, Course: 'MERN', Batch: 'November', }
+  ])
   return (
     <>
-    <BrowserRouter>
-    <div className='header'>
-      <NavLink to='/' className='tag'>Home</NavLink>
-      <NavLink to='/Student' className='tag'>Student</NavLink>
-      <NavLink to='/ContactUs' className='tag'>Contact</NavLink>
-    </div>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/Student' element={
-        <Store.Provider value={{Students : Data , UpdateStd : setData}}>
-          <Student/>
-        </Store.Provider>
-      }/>
-      <Route path='/Contactus' element={<Contactus/>} />
-      <Route path='/AddStudent' element={
-        <Store.Provider value={{Students : Data , UpdateStd : setData}}>
-          <AddNewStudent/>
-        </Store.Provider>} />
+      <BrowserRouter>
+        <div className='header'>
+          <NavLink to='/' className='tag'>Home</NavLink>
+          <NavLink to='/Student' className='tag'>Student</NavLink>
+          <NavLink to='/ContactUs' className='tag'>Contact</NavLink>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Student' element={
+            <Store.Provider value={{ Students: Data, UpdateStd: setData }}>
+              <Student />
+            </Store.Provider>
+          } />
+          <Route path='/Contactus' element={<Contactus />} />
+          <Route path='/AddStudent' element={
+            <Store.Provider value={{ Students: Data, UpdateStd: setData }}>
+              <AddNewStudent />
+            </Store.Provider>} />
 
-        <Route path='/edit' element={
-        <Store.Provider value={{Students : Data , UpdateStd : setData}}>
-          <EditStudent/>
-        </Store.Provider>} />
-    </Routes>
- 
-  
-    </BrowserRouter>
+          <Route path='/edit' element={
+            <Store.Provider value={{ Students: Data, UpdateStd: setData }}>
+              <EditStudent />
+            </Store.Provider>} />
+        </Routes>
+
+
+      </BrowserRouter>
     </>
   )
 }
